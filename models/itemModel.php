39977dacd9503bd6b853d2ddfe2f638e71c0edd5
RamiroCortesMor <developersoftware23@gmail.com>
2025-11-06 21:49:06 -0600

    VIDEO 70 STARTING

---
+++ models/itemModel.php
@@ -0,0 +1,18 @@
+<?php
+    require_once ("mainModel.php");
+
+    class ItemModel extends MainModel{
+        protected function add_item_model($data){
+            $insertItem = MainModel :: connection()->prepare("INSERT INTO item
+                (item_codigo, item_nombre, item_stock, item_estado, item_detalle)
+                VALUES
+                (:CODE, :NAME_, :STOCK, :STATUS_, :DETAILS)");
+            $insertItem->bindParam(":CODE", $data['code']);
+            $insertItem->bindParam(":NAME_", $data['name']);
+            $insertItem->bindParam(":STOCK", $data['status']);
+            $insertItem->bindParam(":DETAILS", $data['details']);
+            $insertItem -> execute();
+            
+            return $insertItem;
+        }
+    }
